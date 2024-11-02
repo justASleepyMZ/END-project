d99fa59374ec1c610f9913de96763c65da3c95e9
Ayaulym Kenzhekul <154233552+ayaulym28@users.noreply.github.com>
2024-11-02 13:20:17 +0500

    Room.java

--- Room.java
+++ Room.java
@@ -1,49 +1,16 @@
-// Abstract class for different types of Rooms
-abstract class Room {
-    protected String type;
-    protected double rate;
+package main;
 
-    public abstract double getRate();
-    public String getType() {
+public abstract class Room {
+    protected java.lang.String type;
+    protected double price;
+
+    public abstract void book();
+
+    public java.lang.String getType() {
         return type;
     }
-}
 
-// Concrete Room Classes
-class SingleRoom extends Room {
-    public SingleRoom() {
-        this.type = "Single";
-        this.rate = 100.0;
-    }
-
-    @Override
-    public double getRate() {
-        return rate;
-    }
-}
-
-class DoubleRoom extends Room {
-    public DoubleRoom() {
-        this.type = "Double";
-        this.rate = 150.0;
-    }
-
-    @Override
-    public double getRate() {
-        return rate;
-    }
-}
-
-// RoomFactory to create different Room objects
-class RoomFactory {
-    public static Room createRoom(String type) {
-        switch (type.toLowerCase()) {
-            case "single":
-                return new SingleRoom();
-            case "double":
-                return new DoubleRoom();
-            default:
-                throw new IllegalArgumentException("Unknown room type.");
-        }
+    public double getPrice() {
+        return price;
     }
 }
