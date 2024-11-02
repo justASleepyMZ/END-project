232602bcf5f3375d6e8b3ac1b3c75ffe56593dbd
Ayaulym Kenzhekul <154233552+ayaulym28@users.noreply.github.com>
2024-11-02 13:18:32 +0500

    BookingFacade.java

--- BookingFacade.java
+++ BookingFacade.java
@@ -1,24 +1,26 @@
-class BookingFacade {
-    private RoomFactory roomFactory;
+package main;
+
+public class BookingFacade {
+    private RoomService roomService;
+    private UserService userService;
     private BookingService bookingService;
-    private PriceContext priceContext;
 
     public BookingFacade() {
-        roomFactory = new RoomFactory();
-        bookingService = new BookingService();
-        priceContext = new PriceContext();
+        this.roomService = new RoomService();
+        this.userService = new UserService();
+        this.bookingService = BookingService.getInstance();
     }
 
-    public void addCustomerToNotifications(Observer customer) {
-        bookingService.addObserver(customer);
+    public void bookRoom(String user, String roomType) {
+        if (userService.isValidUser(user) && roomService.isAvailable(roomType)) {
+            bookingService.book(roomType, user);
+            System.out.println("Room booked successfully for " + user);
+        } else {
+            System.out.println("Booking failed.");
+        }
     }
 
-    public void bookRoom(String roomType, PricingStrategy strategy) {
-        Room room = roomFactory.createRoom(roomType);
-        priceContext.setStrategy(strategy);
-        double finalPrice = priceContext.getFinalPrice(room);
-
-        System.out.println("Booking a " + roomType + " room at $" + finalPrice);
-        bookingService.bookRoom(room);
+    public RoomService getRoomService() {
+        return roomService;
     }
 }
