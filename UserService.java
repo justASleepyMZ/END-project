9694ebd3eedf177d680b4b4973fd765ce9958b38
Ayaulym Kenzhekul <154233552+ayaulym28@users.noreply.github.com>
2024-11-02 13:20:53 +0500

    UserService.java

---
+++ UserService.java
@@ -0,0 +1,8 @@
+package main;
+
+public class UserService {
+    public boolean isValidUser(String user) {
+        // Assume user validation logic (stubbed for simplicity)
+        return true;
+    }
+}
