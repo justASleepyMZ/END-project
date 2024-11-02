0d1cce4ab12209c22459f04ae0675b7bdccff570
Ayaulym Kenzhekul <154233552+ayaulym28@users.noreply.github.com>
2024-11-02 13:23:30 +0500

    Customer.java

---
+++ Customer.java
@@ -0,0 +1,18 @@
+package main;
+
+public class Customer implements Observer {
+    private String name;
+
+    public Customer(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void update(String message) {
+        System.out.println("Notification for " + name + ": " + message);
+    }
+
+    public String getName() {
+        return name;
+    }
+}
